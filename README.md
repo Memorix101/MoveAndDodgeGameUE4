b435c19540359dfcba09b7f2c7fdf0401b5e7775
Memorix101 <memorix101@gmail.com>
2020-02-23 15:11:12 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MoveAndDodgeGameUE4
 Little roll a ball game in Unreal Engine 4 using blueprints visual scripting
+
+Made with Unreal Engine 4.24.2
+
+![](https://i.imgur.com/eWb5cZc.png)
---
+++ Source/MoveAndDodgeGame/MoveAndDodgeGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MoveAndDodgeGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOVEANDDODGEGAME_API AMoveAndDodgeGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
